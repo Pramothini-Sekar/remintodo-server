31024a3e381eefb6f4a45dfb42b563a3e97d2d33
Pramothini Sekar <13pramo@gmail.com>
2021-11-06 01:47:43 -0400

    P0: Added user collection

--- app.py
+++ app.py
@@ -23,6 +23,7 @@ auth_token = os.environ['TWILIO_AUTH_TOKEN']
 api_key = os.environ['API_KEY']
 api_secret = os.environ['API_SECRET']
 
+
 app.secret_key = api_secret
 client = Client(account_sid, auth_token)
 
@@ -31,17 +32,60 @@ if not firebase_admin._apps:
     cred = credentials.Certificate('google-credentials.json')
     default_app = initialize_app(cred)
 db = firestore.client()
-todo_ref = db.collection('todos')
-motivator_ref = db.collection('motivators')
+user_ref = db.collection('users')
 
 # A welcome message to test our server
 @app.route('/')
 def index():
     return "<h1>Welcome to our Remintodo !!</h1>"
 
+#Adding user phone number to Twilio Verified Users
+def verifyTwilioPhoneNum(phoneNum):
+    phoneNumberWithCode = '+1' + phoneNum
+    validation_request = client.validation_requests \
+                           .create(
+                                friendly_name='My Home Phone Number',
+                                phone_number=phoneNumberWithCode
+                            )
+                          
+@app.route('/add-user', methods=['POST'])
+def add_user():
+    document_id = request.json['number']
 
-@app.route('/add', methods=['POST'])
-def create():
+    try:
+        user_ref.document(document_id).set(request.json)
+        all_users = [doc.to_dict() for doc in user_ref.stream()]
+        verifyTwilioPhoneNum(document_id)
+        return jsonify(all_users), 200
+    except Exception as e:
+        return f"An Error Occured: {e}"
+    
+@app.route('/user', methods=['GET'])
+def fetch_user():
+    try:
+        # Check if ID was passed to URL query
+        user_id = request.args.get('number')
+        user = user_ref.document(user_id).get()
+        return jsonify(user.to_dict()), 200
+    except Exception as e:
+        return f"An Error Occured: {e}"
+
+ 
+@app.route('/users', methods=['GET'])
+def fetch_all_user():
+    try:
+        # Check if ID was passed to URL query
+        all_users = [doc.to_dict() for doc in user_ref.stream()]
+        return jsonify(all_users), 200
+    except Exception as e:
+        return f"An Error Occured: {e}"
+    
+@app.route('/update-user',  methods=['POST', 'PUT'])
+def update_user():
+    pass
+    
+@app.route('/<number>/add', methods=['POST'])
+def create(number):
     """
         create() : Add document to Firestore collection with request body.
         Ensure you pass a custom ID as part of json body in post request,
@@ -49,14 +93,15 @@ def create():
     """
     try:
         id = request.json['id']
+        todo_ref = user_ref.document(number).collection("todos")
         todo_ref.document(id).set(request.json)
         all_todos = [doc.to_dict() for doc in todo_ref.stream()]
         return jsonify(all_todos), 200
     except Exception as e:
         return f"An Error Occured: {e}"
 
-@app.route('/list', methods=['GET'])
-def read():
+@app.route('/<number>/list', methods=['GET'])
+def read(number):
     """
         read() : Fetches documents from Firestore collection as JSON.
         todo : Return document that matches query ID.
@@ -66,22 +111,25 @@ def read():
         # Check if ID was passed to URL query
         todo_id = request.args.get('id')
         if todo_id:
+            todo_ref = user_ref.document(number).collection("todos")
             todo = todo_ref.document(todo_id).get()
             return jsonify(todo.to_dict()), 200
         else:
+            todo_ref = user_ref.document(number).collection("todos")
             all_todos = [doc.to_dict() for doc in todo_ref.stream()]
             return jsonify(all_todos), 200
     except Exception as e:
         return f"An Error Occured: {e}"
 
-@app.route('/update', methods=['POST', 'PUT'])
-def update():
+@app.route('/<number>/update', methods=['POST', 'PUT'])
+def update(number):
     """
         update() : Update document in Firestore collection with request body.
         Ensure you pass a custom ID as part of json body in post request,
         e.g. json={'id': '1', 'title': 'Write a blog post today'}
     """
     try:
+        todo_ref = user_ref.document(number).collection("todos")
         id = request.json['id']
         todo_ref.document(id).update(request.json)
         all_todos = [doc.to_dict() for doc in todo_ref.stream()]
@@ -89,12 +137,13 @@ def update():
     except Exception as e:
         return f"An Error Occured: {e}"
 
-@app.route('/delete', methods=['GET', 'DELETE'])
-def delete():
+@app.route('/<number>/delete', methods=['GET', 'DELETE'])
+def delete(number):
     """
         delete() : Delete a document from Firestore collection.
     """
     try:
+        todo_ref = user_ref.document(number).collection("todos")
         # Check for ID in URL query
         todo_id = request.args.get('id')
         todo_ref.document(todo_id).delete()
@@ -103,14 +152,15 @@ def delete():
     except Exception as e:
         return f"An Error Occured: {e}"
     
-@app.route('/add-motivator', methods=['POST'])
-def create_motivator():
+@app.route('/<number>/add-motivator', methods=['POST'])
+def create_motivator(number):
     """
         create() : Add document to Firestore collection with request body.
         Ensure you pass a custom ID as part of json body in post request,
         e.g. json={'id': '1', 'title': 'Write a blog post'}
     """
     try:
+        motivator_ref = user_ref.document(number).collection("motivators")
         id = request.json['id']
         motivator_ref.document(id).set(request.json)
         all_motivators = [doc.to_dict() for doc in motivator_ref.stream()]
@@ -118,31 +168,34 @@ def create_motivator():
     except Exception as e:
         return f"An Error Occured: {e}"
     
-@app.route('/list-motivator', methods=['GET'])
-def read_motivator():
+@app.route('/<number>/list-motivator', methods=['GET'])
+def read_motivator(number):
     """
         read() : Fetches documents from Firestore collection as JSON.
         todo : Return document that matches query ID.
         all_todos : Return all documents.
     """
     try:
+        motivator_ref = user_ref.document(number).collection("motivators")
         # Check if ID was passed to URL query
         motivator_id = request.args.get('id')
         if motivator_id:
             motivator = motivator_ref.document(motivator_id).get()
             return jsonify(motivator.to_dict()), 200
         else:
+            motivator_ref = user_ref.document(number).collection("motivators")
             all_motivators = [doc.to_dict() for doc in motivator_ref.stream()]
             return jsonify(all_motivators), 200
     except Exception as e:
         return f"An Error Occured: {e}"
     
-@app.route('/delete-motivator', methods=['GET', 'DELETE'])
-def delete_motivator():
+@app.route('/<number>/delete-motivator', methods=['GET', 'DELETE'])
+def delete_motivator(number):
     """
         delete() : Delete a document from Firestore collection.
     """
     try:
+        motivator_ref = user_ref.document(number).collection("motivators")
         # Check for ID in URL query
         motivator_id = request.args.get('id')
         motivator_ref.document(motivator_id).delete()
